bc18ca9f4ce74111c3a6f27ded0e650b16cee9e6
TahidFlexion <163385747+TahidFlexion@users.noreply.github.com>
2024-05-11 16:22:42 +0530

    Done

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -14,7 +14,7 @@ function Services() {
             </div>
             <div className="container py-5">
                 <div className="d-flex justify-content-center align-items-center gap-5 text-center">
-                    <div className="col-xxl-4 col-xl-5 col-lg-6 col-md-5 col-sm-12 col-12  box pt-xxl-4 pt-xl-4 pt-lg-4 pt-md-3 pt-sm-3 pt-2 text-center services-main">
+                    <div className="col-xxl-4 col-xl-5 col-lg-6 col-md-6 col-sm-12 col-12  box pt-xxl-4 pt-xl-4 pt-lg-4 pt-md-3 pt-sm-3 pt-2 text-center services-main">
                         <div className="col-12  p-xxl-4 p-xl-4 p-lg-4 p-md-3 p-sm-2 p-2">
                             <div className="services-content">
                                 <div className="icons">
@@ -35,7 +35,7 @@ function Services() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-xxl-4 col-xl-5 col-lg-6 col-md-5 col-sm-12 col-11  box-second pt-xxl-4 pt-xl-4 pt-lg-4 pt-md-3 pt-sm-3 pt-2 text-center services-main ">
+                    <div className="col-xxl-4 col-xl-5 col-lg-6 col-md-6 col-sm-12 col-11  box-second pt-xxl-4 pt-xl-4 pt-lg-4 pt-md-3 pt-sm-3 pt-2 text-center services-main ">
                         <div className="col-12  p-xxl-4 p-xl-4 p-lg-4 p-md-3 p-sm-2 p-2 ">
                             <div className="services-content">
                                 <div className="block-img">
@@ -63,7 +63,7 @@ function Services() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-xxl-4 col-xl-5 col-lg-6  col-md-5 col-sm-12 col-12  box pt-xxl-4 pt-xl-4 pt-lg-4 pt-md-3 pt-sm-3 pt-2 text-center services-main">
+                    <div className="col-xxl-4 col-xl-5 col-lg-6  col-md-6 col-sm-12 col-12  box pt-xxl-4 pt-xl-4 pt-lg-4 pt-md-3 pt-sm-3 pt-2 text-center services-main">
                         <div className="col-12  p-xxl-4 p-xl-4 p-lg-4 p-md-3 p-sm-2 p-2">
                             <div className="services-content">
                             <div className="icons">
